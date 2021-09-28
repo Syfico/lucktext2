e641facb8dc4a78be165bd1c2342084a2f8d950a
Albin Brolin <crystallizedtester@gmail.com>
2021-09-28 14:46:59 +0200

    kdshjfkhsdkfhdskjfhkdjhfkjdhfkjsdhfkjh

---
+++ lucktext2/Program.cs
@@ -0,0 +1,128 @@
+﻿
+/*using System.Dynamic;
+using System;
+
+string name;
+string weapon;
+
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("-Shalom främling, hur lyder ditt namn?");
+Console.WriteLine();
+
+name = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("-Hej, " + name + ", fint väder vi har idag, inte sant?");
+Console.WriteLine();
+Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("...");
+Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("-Eller hur?");
+Console.WriteLine();
+
+Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("-Säg mig, jag skulle vilja fråga dig om en sak, " + name + ".");
+Console.WriteLine();
+Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("...");
+Console.WriteLine();
+Console.Clear();
+Console.WriteLine();
+
+//Console.ReadLine();//
+Console.WriteLine("En teoretisk fråga, vad skulle du säga är det bästa mordvapnet?");
+Console.WriteLine("Ja, alltså, jag letar efter något somm är lätt att hitta, lätt att gömma som inte kostar mig skjortan.");
+Console.WriteLine();
+
+weapon = Console.ReadLine();
+
+Console.ReadLine();
+Console.WriteLine("Tja, en " + weapon + " skulle kanske funka?");
+Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("-Ja en " + weapon + " är tämligen lätt att få tag på. Bra där!");
+System.Console.WriteLine();
+Console.ReadLine();
+
+Console.WriteLine(weapon + name);
+
+
+//" det perfekta mordvapnet"
+
+//Console.ReadLine();
+//Console.ReadLine("");//*/
+
+
+
+using System.Dynamic;
+using System;
+
+string name;
+string answer1;
+
+answer1 = Console.ReadLine();
+name = Console.ReadLine();
+
+Console.WriteLine($"Hej främling, hur lyder ditt namn?");
+name = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"Hej,  {name}" );
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine();
+Console.WriteLine("Du får ursäkta mig, men jag är ivrig att få lära känna dig.");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine();
+Console.WriteLine("Det var länge någon annan än jag befann sig så högt upp och så långt bort från civilisationen.");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine();
+Console.WriteLine("Hur som helst, jag vill spela ett spel. Inte ett stort spel, åh nej. Men ett litet.");
+Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine($"Säg mig, är du intresserad, {name}?");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"{answer1}{name}?");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine();
+Console.WriteLine("...");
+Console.WriteLine();
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine();
+Console.WriteLine("Det svaret duger utmärkt.");
+Console.WriteLine();
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
